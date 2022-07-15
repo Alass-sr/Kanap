5be4bd2a6f7ca52eda165f2d845767a0318c2abd
Alass-sr <sarralass88@gmail.com>
2022-07-15 15:19:49 +0200

    correction des erreurs

--- js/product.js
+++ js/product.js
@@ -1,71 +1,66 @@
 let article_url_id = window.location.search;
 
-let url_Id = new URLSearchParams (article_url_id);  
+let urlParams = new URLSearchParams(article_url_id);
+let productId = urlParams.get("id");
+
+function setColors(colors) {
+  for (let color of colors) {
+    let select = document.getElementById("colors");
+    let option = document.createElement("option");
+
+    option.value = color;
+    option.textContent = color;
+    select.appendChild(option);
+  }
+
+}
 
 
 function choiceProd(article) {
   let item = document.querySelector(".item");
-  item.className = "item__img";
 
-  let choiceArticle = item.querySelector("article");
-  
   let itemImg = item.querySelector(".item__img");
   let image = document.createElement("img");
-  image.className = "item__img"
   image.src = article.imageUrl;
   image.alt = article.altTxt;
 
-
-  let choiceContent = item.querySelector(".item__content");
-
-  let productName = item.querySelector("h1");
-  productName.className = "item__content__titlePrice";
+  let productName = item.querySelector("#title");
   productName.textContent = article.name;
 
-  let prodPrice = item.querySelector("span");
-  prodPrice.className = "item__content__titlePrice";
+  let prodPrice = item.querySelector("#price");
   prodPrice.textContent = article.price;
 
-  let prodDescription = item.querySelector("p");
-  prodDescription.className = "item__content__description";
+  let prodDescription = item.querySelector("#description");
   prodDescription.textContent = article.description;
 
 
-  item.appendChild(choiceArticle);
+  // setColors(article.colors);
 
-  choiceArticle.appendChild(itemImg);
+ 
   itemImg.appendChild(image);
-  choiceContent.appendChild(productName);
-  choiceContent.appendChild(prodPrice);
-  choiceContent.appendChild(prodDescription);
+  
 
 
-
-  return item;
 }
-
 
 // choiceProd();
 
 async function getProducts() {
 
-  let id = (new URL(window.location).searchParams.get("id"));
+  let id = new URL(window.location).searchParams.get("id");
 
-  await fetch("http://localhost:3000/api/products/"+ id)
+  await fetch("http://localhost:3000/api/products/" + productId)
     .then((response) => response.json())
     .then((data) => {
-       choiceProd(data);
+      console.log(data);
+      
 
-       let articleProd = document.getElementsByClassName("item");
-      for (let article of data) {
-        let item = choiceProd(article);
-        articleProd.appendChild(item);
+      
         
-      }
-       console.log(data);
-
-
+        choiceProd(data);
+        
+      
     });
 }
 
- getProducts();
+getProducts();
